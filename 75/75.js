09a3d31e1ff00bdb1726b9e383c338caa0190728
Zac <zacace@ingoglia.com>
2021-05-19 14:26:55 -0700

    Finished 75

--- 75/75.js
+++ 75/75.js
@@ -1,30 +1,51 @@
-const sortColors = (nums) => {
-  let zeroArr = [];
-  let oneArr = [];
-  let twoArr = [];
-  let result = [];
-  nums.forEach((elem) => {
-    if (elem === 0) {
-      zeroArr.push(elem);
+let sortColors = (nums) => {
+  // Get Max Val
+  let max;
+  nums.forEach((num, index) => {
+    if (index === 0) {
+      max = num;
+      return;
     }
-    if (elem === 1) {
-      oneArr.push(elem);
+    if (num > max) {
+      max = num;
+      return;
     }
-    if (elem === 2) {
-      twoArr.push(elem);
+    return;
+  });
+
+  // Get Min Val
+  let min;
+  nums.forEach((num, index) => {
+    if (index === 0) {
+      min = num;
+      return;
     }
+    if (num < min) {
+      min = num;
+      return;
+    }
+    return;
   });
-  zeroArr.forEach((elem) => {
-    result.push(elem);
-  });
-  oneArr.forEach((elem) => {
-    result.push(elem);
-  });
-  twoArr.forEach((elem) => {
-    result.push(elem);
-  });
-  console.log(nums);
-  return result;
+
+  // Sort using the min and max as ref points
+  let i = min,
+    j = 0,
+    length = nums.length,
+    count = [];
+  for (i; i <= max; i++) {
+    count[i] = 0;
+  }
+  for (i = 0; i < length; i++) {
+    count[nums[i]] += 1;
+  }
+  for (i = min; i <= max; i++) {
+    while (count[i] > 0) {
+      nums[j] = i;
+      j++;
+      count[i]--;
+    }
+  }
+  return nums;
 };
 
-console.log(sortColors([2, 0, 2, 1, 1, 0]));
+console.log(sortV3([2, 0, 2, 1, 1, 0]));
